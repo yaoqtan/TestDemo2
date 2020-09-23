cd24c610467bcb55cd9eac739e9c8b30571d1c2a
yaoqingtan <1148559020@qq.com>
2020-09-23 11:42:29 +0800

    update

---
+++ 007-javascript面试视频/webpack/src/app.js
@@ -0,0 +1,10 @@
+// var $ = require('jquery')
+//
+// console.log($)
+//
+// console.log(100)
+console.log(process.env.NODE_ENV)
+
+console.log(process.env.NODE_ENV)
+var aUtil=require('./a-util.js')
+aUtil.print()

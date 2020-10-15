30b0a4def892fafce568d8ecb5b8e29b2f1caa15
yaoqingtan <1148559020@qq.com>
2020-10-16 00:14:04 +0800

    update

--- vue-debounce/src/main.js
+++ vue-debounce/src/main.js
@@ -3,8 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/style.css'
+import vuescroll from 'vuescroll';
 //Vue.use(scroll)
 //Vue.use(scroll,{componentName: 'scroll-seamless'})
+
+
+Vue.use(vuescroll); // install the vuescroll first
+Vue.prototype.$vuescrollConfig = {
+  bar: {
+    background: '#000'
+  }
+};
 
 Vue.config.productionTip = false
 
